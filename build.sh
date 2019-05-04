8331f5e5d69ff9a202fac2b816f40e9b796a351c
Rick Henry <fredericmhenry@gmail.com>
2019-05-04 11:37:46 -0400

    try to add indexing
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ do
     sed -i "/^tags:/s/'$//g" $file
 done
 pelican content
+yarn run para-cli "output/**/*.html" --type "blogpost" --sanitize --accessKey $PARA_ACCESS_KEY --secretKey $PARA_SECRET_KEY

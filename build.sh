1fb8eb801ac6517056ee96acc433080bb9a605bb
Rick Henry <fredericmhenry@gmail.com>
2019-05-04 11:48:40 -0400

    add more specific indexing
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- build.sh
+++ build.sh
@@ -5,4 +5,7 @@ do
     sed -i "/^tags:/s/'$//g" $file
 done
 pelican content
-yarn run para-cli create "output/**/*.html" --endpoint https://paraio.com --type "blogpost" --sanitize --accessKey $PARA_ACCESS_KEY --secretKey $PARA_SECRET_KEY
+yarn run para-cli create "output/20*.html" --endpoint https://paraio.com --type "article" --sanitize --accessKey $PARA_ACCESS_KEY --secretKey $PARA_SECRET_KEY
+yarn run para-cli create "output/tag/*.html" --endpoint https://paraio.com --type "tag" --sanitize --accessKey $PARA_ACCESS_KEY --secretKey $PARA_SECRET_KEY
+yarn run para-cli create "output/category/*.html" --endpoint https://paraio.com --type "category" --sanitize --accessKey $PARA_ACCESS_KEY --secretKey $PARA_SECRET_KEY
+yarn run para-cli create "output/author/*.html" --endpoint https://paraio.com --type "author" --sanitize --accessKey $PARA_ACCESS_KEY --secretKey $PARA_SECRET_KEY
